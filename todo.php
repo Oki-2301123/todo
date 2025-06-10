c0737532694705ce4e2270da46fcc5e2e85b1eb5
aso-ryo <2301181@s.asojuku.ac.jp>
2025-06-10 11:00:29 +0900

    todo作成

---
+++ todo.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ToDoリスト</title>
+</head>
+<body>
+
+<h1>ToDoリスト</h1>
+
+
+
+    <h2>タスク追加</h2>
+    <input type="text" name="task" placeholder="タスク内容"> // タスク追加のテキストボックス
+    <input type="date" name="y/m/d"> //タスク追加の日付入力欄
+    <select name='priority'> //タスク追加の優先度選択欄(プルダウン)        
+        <option value="1">優先度(低)</option>
+        <option value="2">優先度(中)</option>
+        <option value="3">優先度(高)</option>
+</select>
+<button>適用</button>
+
+
+
+    <h2>フィルタ/検索</h2>
+    <input type="text" name="filter" placeholder="キーワード"> // フィルタ/検索のテキストボックス
+    <input type="date" name="y/m/d"> //フィルタ/検索の日付入力欄
+    <select name='priority'> //フィルタ/検索の優先度選択欄(プルダウン)        
+        <option value="1">優先度(全て)</option>
+        <option value="2">優先度(低)</option>
+        <option value="3">優先度(中)</option>
+        <option value="3">優先度(高)</option>
+</select>
+<button>適用</button>
+</body>
+</html>
