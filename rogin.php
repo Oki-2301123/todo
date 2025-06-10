41ece0110d1af25d0b01ec92153f4c1f2c1cd51d
2301133@s.asoujuku.ac.jp <2301133@s.asojuku.ac.jp>
2025-06-10 10:58:05 +0900

    ログイン

---
+++ rogin.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ログイン</title>
+</head>
+<body>
+    <h2>ログイン</h2>
+    <form action="check.php" method="post">
+    ユーザー名：<input type="text" name="username"><br>
+    パスワード：<input type="password" name="password"><br>
+    <input type="submit" value="ログイン">
+    </form>
+
+    <p><a href="register.php">新規登録</a></p>
+</body>
+</html>
