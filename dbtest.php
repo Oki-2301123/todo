06944485c40372e3bcf72ce6bfbb5204f0b1062d
Oki-2301123 <2301123@s.asojuku.ac.jp>
2025-06-11 11:27:51 +0900

    a

---
+++ dbtest.php
@@ -0,0 +1,23 @@
+<?php
+require_once 'function.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $pdo = DB();
+
+    $stmt = $pdo->query("SELECT * FROM users");
+
+    // データを表示
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        echo "<p>名前: " . htmlspecialchars($row['name'], ENT_QUOTES, 'UTF-8') . "</p>";
+    }
+    ?>
+</body>
+</html>
