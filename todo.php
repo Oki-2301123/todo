32d97328d8bf8f1b5cdef2bc4eb3e923cf2fd16d
aso-ryo <2301181@s.asojuku.ac.jp>
2025-06-11 12:36:28 +0900

    変更

--- todo.php
+++ todo.php
@@ -8,13 +8,13 @@
 <body>
 
 <h1>ToDoリスト</h1>
-
-
+<!-- ログイン者の名前
+ 　　<a href="#">ログアウト</a> -->
 
     <h2>タスク追加</h2>
-    <input type="text" name="task" placeholder="タスク内容"> // タスク追加のテキストボックス
-    <input type="date" name="y/m/d"> //タスク追加の日付入力欄
-    <select name='priority'> //タスク追加の優先度選択欄(プルダウン)        
+    <input type="text" name="task" placeholder="タスク内容"　required>> // タスク追加のテキストボックス
+    <input type="date" name="y/m/d"　required>> //タスク追加の日付入力欄
+    <select name='priority'　required>> //タスク追加の優先度選択欄(プルダウン)        
         <option value="1">優先度(低)</option>
         <option value="2">優先度(中)</option>
         <option value="3">優先度(高)</option>
@@ -24,14 +24,21 @@
 
 
     <h2>フィルタ/検索</h2>
-    <input type="text" name="filter" placeholder="キーワード"> // フィルタ/検索のテキストボックス
-    <input type="date" name="y/m/d"> //フィルタ/検索の日付入力欄
-    <select name='priority'> //フィルタ/検索の優先度選択欄(プルダウン)        
+    <input type="text" name="filter" placeholder="キーワード"　required>> // フィルタ/検索のテキストボックス
+    <input type="date" name="y/m/d"　required>> //フィルタ/検索の日付入力欄
+    <select name='priority'　required>> //フィルタ/検索の優先度選択欄(プルダウン)        
         <option value="1">優先度(全て)</option>
         <option value="2">優先度(低)</option>
         <option value="3">優先度(中)</option>
         <option value="3">優先度(高)</option>
 </select>
 <button>適用</button>
+
+<th>状態</th>
+<th>タスク</th>
+<th>期限</th>
+<th>優先度</th>
+<th>操作</th>
+
 </body>
 </html>
