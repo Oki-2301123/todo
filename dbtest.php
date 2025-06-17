ef65def37bfd42c62462e5acb899085234a9a37a
Oki-2301123 <2301123@s.asojuku.ac.jp>
2025-06-17 09:44:24 +0900

    dbtest

--- dbtest.php
+++ dbtest.php
@@ -16,7 +16,11 @@ require_once 'function.php';
 
     // データを表示
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        echo "<p>ID: " . htmlspecialchars($row['id'], ENT_QUOTES, 'UTF-8') . "</p>";
         echo "<p>名前: " . htmlspecialchars($row['username'], ENT_QUOTES, 'UTF-8') . "</p>";
+        echo "<p>パスワード: " . htmlspecialchars($row['password'], ENT_QUOTES, 'UTF-8') . "</p>";
+        echo "<p>作成日: " . htmlspecialchars($row['created_at'], ENT_QUOTES, 'UTF-8') . "</p>";
+
     }
     ?>
 </body>
