eb47e2757699efe5407fc64361d4f75bfdbffe4b
murakami722 <2301208@s.asojuku.ac.jp>
2025-06-17 10:08:38 +0900

    signup

--- signup.php
+++ signup.php
@@ -9,8 +9,8 @@
 
 <body>
     <h1>ユーザー登録</h1>
-    <form action="rogin.php" method="post">
-        ユーザー名:<input type="text" name="username"><br>
+    <form action="signupdb.php" method="post">
+        ユーザー名:<input type="text" name="user"><br>
         パスワード:<input type="password" name="password"><br>
         <button type="submit">登録</button><br><br>
         <a href="rogin.php">ログインはこちら</a>
