e58a58ae6e2d3b7a5daa4b0d529e735cefb86a59
aso-tanigawa <2301175@s.asojuku.ac.jp>
2025-06-17 10:24:23 +0900

    ほぼ完成

--- edit.php
+++ edit.php
@@ -8,7 +8,6 @@
 <body>
     <h1>タスク編集</h1>
 <?php
-/*
     $pdo = new PDO(
         'mysql:host=mysql304.phy.lolipop.lan;
         dbname=LAA1554903-php2024;charset=utf8',
@@ -24,7 +23,6 @@
     $sql->execute([$name,$day,$priority,$status]);
     $pdo = null;
     echo 'データ転送完了';
-*/
 ?>
 <form action="#" method="post">
     <dl>
