b07a824e72f09b0a44a0f11716a5c300199ad3a9
murakami722 <2301208@s.asojuku.ac.jp>
2025-07-01 10:36:32 +0900

    signup.css

--- signup.php
+++ signup.php
@@ -5,16 +5,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="./css/signup.css">
 </head>
-
 <body>
+    <div class="tt">
     <h1>ユーザー登録</h1>
+    </div>
+    <div class="signup">
     <form action="signupdb.php" method="post">
         ユーザー名:<input type="text" name="user"><br>
         パスワード:<input type="password" name="password"><br>
-        <button type="submit">登録</button><br><br>
-        <a href="rogin.php">ログインはこちら</a>
+        <input type="submit" value="登録"></input><br><br>
+</div>
+        <a href="login.php">ログインはこちら</a>
     </form>
 </body>
-
 </html>
